6ab797f2651f8563bde9480146dddadf1cab1411
Anshal <lad.anshal97@gmail.com>
2017-05-20 03:35:57 +0530

    First app

---
+++ app/src/main/java/com/example/anshallad/testapp/Lay.java
@@ -0,0 +1,29 @@
+package com.example.anshallad.testapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Lay extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lay);
+
+
+        Button btn = (Button) findViewById(R.id.submit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Lay.this,Second.class));
+            }
+        });
+
+
+        
+    }
+}
